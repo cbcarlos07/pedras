6e395a9082e26c615f2834111298dccb12f4f8b0
cbcarlos07 <cbcarlos08@gmail.com>
2017-07-16 18:26:20 -0400

    Projeto Pedras- Criando Lists e Iterações

---
+++ services/class.cargoListIterator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 17/02/17
+ * Time: 17:02
+ */
+class cargoListIterator
+{
+    protected $cargoList;
+    protected $currentCargo = 0;
+
+    public function __construct(cargoList $cargoList_in) {
+        $this->cargoList = $cargoList_in;
+    }
+    public function getCurrentCargo() {
+        if (($this->currentCargo > 0) &&
+            ($this->cargoList->getCargoCount() >= $this->currentCargo)) {
+            return $this->cargoList->getCargo($this->currentCargo);
+        }
+    }
+    public function getNextCargo() {
+        if ($this->hasNextCargo()) {
+            return $this->cargoList->getCargo(++$this->currentCargo);
+        } else {
+            return NULL;
+        }
+    }
+    public function hasNextCargo() {
+        if ($this->cargoList->getCargoCount() > $this->currentCargo) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+}
---
+++ services/class.cidadeListIterator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 17/02/17
+ * Time: 17:02
+ */
+class cidadeListIterator
+{
+    protected $cidadeList;
+    protected $currentCidade = 0;
+
+    public function __construct(cidadeList $cidadeList_in) {
+        $this->cidadeList = $cidadeList_in;
+    }
+    public function getCurrentCidade() {
+        if (($this->currentCidade > 0) &&
+            ($this->cidadeList->getCidadeCount() >= $this->currentCidade)) {
+            return $this->cidadeList->getCidade($this->currentCidade);
+        }
+    }
+    public function getNextCidade() {
+        if ($this->hasNextCidade()) {
+            return $this->cidadeList->getCidade(++$this->currentCidade);
+        } else {
+            return NULL;
+        }
+    }
+    public function hasNextCidade() {
+        if ($this->cidadeList->getCidadeCount() > $this->currentCidade) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+}
