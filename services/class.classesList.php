7754398786c9903e8f40d722dbfc054fcd8bb7a2
cbcarlos07 <cbcarlos08@gmail.com>
2017-07-18 20:53:35 -0400

    Projeto Pedras- Criando Lists e Iterações

--- services/class.classesList.php
+++ services/class.classesList.php
@@ -49,7 +49,7 @@ class classesList
             return null;
         }
     }
-    
+
 
     public function addClasses(Classes $_classes_in) {
         $this->setClassesCount($this->getClassesCount() + 1);
